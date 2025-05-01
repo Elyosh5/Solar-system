5e32e2e4e61a883f262560221d95ce301086cd45
Eohan <elamanju5@gmail.com>
2025-05-01 22:57:05 +0600

    FourthCommit

--- main.js
+++ main.js
@@ -15,6 +15,7 @@ import neptuneTexture from "./src/img/neptune.jpg";
 import plutoTexture from "./src/img/pluto.jpg";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { FirstPersonControls } from "three/addons/controls/FirstPersonControls.js";
+import backgroundMusic from "./interstellar.mp3";
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -183,7 +184,11 @@ function handleSpeedUpClick() {
 function handleSlowDownClick() {
   spedUpClicked /= 2;
 }
+
 let playMusic = true;
+
+const audio = new Audio(backgroundMusic);
+audio.loop = true;
 
 function animate() {
   // Rest of your existing animation code...
@@ -193,9 +198,6 @@ function animate() {
   }
 
   renderer.render(scene, camera);
-  if (playMusic) {
-    document.querySelector("audio").play();
-  }
 
   if (isStarted) {
     sun.rotateY(0.0005 * spedUpClicked);
@@ -261,9 +263,6 @@ function animate() {
   pluto.planetObj.position.set(0, 0, 0);
   pluto.planet.position.set(3200, 0, 0);
   renderer.render(scene, camera);
-  if (playMusic) {
-    document.querySelector("audio").play();
-  }
 }
 renderer.setAnimationLoop(animate);
 
@@ -276,6 +275,49 @@ document
   .querySelector("#slowDown")
   .addEventListener("click", handleSlowDownClick);
 
-document.querySelector("#musicPlayer").addEventListener("click", () => {
-  playMusic = false;
+// document.querySelector("#volumeIcon").addEventListener("click", () => {
+//   playMusic = !playMusic;
+//   playMusic
+//     ? document.querySelector("audio").play()
+//     : document.querySelector("audio").pause();
+// });
+
+// window.addEventListener("load", () => {
+//   playMusic = true;
+// });
+
+window.addEventListener("load", () => {
+  const playPromise = audio.play();
+
+  if (playPromise !== undefined) {
+    playPromise.catch((error) => {
+      console.log("Autoplay prevented:", error);
+      playMusic = false;
+    });
+  }
+});
+
+document.querySelector("#volumeIcon").addEventListener("click", () => {
+  playMusic = !playMusic;
+
+  // Update icon
+  const icon = document.querySelector("#volumeIcon");
+  icon.classList.toggle("volume-off");
+  icon.classList.toggle("volume-on");
+
+  // Change the path data
+  const path = icon.querySelector("path");
+  if (playMusic) {
+    path.setAttribute(
+      "d",
+      "M3 9v6h4l5 5V4L7 9H3zm13.5 3c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.85-5 6.71v2.06c4.01-.91 7-4.49 7-8.77s-2.99-7.86-7-8.77z"
+    );
+    audio.play();
+  } else {
+    path.setAttribute(
+      "d",
+      "M16.5 12c0-1.77-1.02-3.29-2.5-4.03v2.21l2.45 2.45c.03-.2.05-.41.05-.63zm2.5 0c0 .94-.2 1.82-.54 2.64l1.51 1.51C20.63 14.91 21 13.5 21 12c0-4.28-2.99-7.86-7-8.77v2.06c2.89.86 5 3.54 5 6.71zM4.27 3L3 4.27 7.73 9H3v6h4l5 5v-6.73l4.25 4.25c-.67.52-1.42.93-2.25 1.18v2.06c1.38-.31 2.63-.95 3.69-1.81L19.73 21 21 19.73l-9-9L4.27 3zM12 4L9.91 6.09 12 8.18V4z"
+    );
+    audio.pause();
+  }
 });
